ef7ab3531c9a66f94a6669a1ccdc5cd733e4f8ef
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 18:54:37 +0100

    Add references

---
+++ client/app.js
@@ -0,0 +1,8 @@
+// References to HTML elements
+
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection = document.querySelector('#messages-section');
+const messagesList = document.querySelector('#messages-list');
+const addMessageForm = document.querySelector('#add-messages-form');
+const userNameInput = document.querySelector('#username');
+const messageContentInput = document.querySelector('#message-content');
