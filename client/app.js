0f4edd601731f4a4fe29ac0ab2fd65da42544c7a
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 19:25:05 +0100

    Add addMessage function; code formatting

--- client/app.js
+++ client/app.js
@@ -1,41 +1,55 @@
 // References to HTML elements
 
-const loginForm = document.querySelector('#welcome-form');
-const messagesSection = document.querySelector('#messages-section');
-const messagesList = document.querySelector('#messages-list');
-const addMessageForm = document.querySelector('#add-messages-form');
-const userNameInput = document.querySelector('#username');
-const messageContentInput = document.querySelector('#message-content');
+const loginForm = document.querySelector("#welcome-form");
+const messagesSection = document.querySelector("#messages-section");
+const messagesList = document.querySelector("#messages-list");
+const addMessageForm = document.querySelector("#add-messages-form");
+const userNameInput = document.querySelector("#username");
+const messageContentInput = document.querySelector("#message-content");
 
 // Global variables
 
-let userName = '';
+let userName = "";
 
 // Functions
 
 const login = (e) => {
-    e.preventDefault();
-    if (userNameInput.value.length > 0) {
-        userName = userNameInput.value;
-        loginForm.classList.remove('show');
-        messagesSection.classList.add('show');
-        addMessageForm.classList.add('show');
-    } else {
-        alert('Please enter a username');
-    }
+  e.preventDefault();
+  if (userNameInput.value.length > 0) {
+    userName = userNameInput.value;
+    loginForm.classList.remove("show");
+    messagesSection.classList.add("show");
+    addMessageForm.classList.add("show");
+  } else {
+    alert("Please enter a username");
+  }
 };
 
 const sendMessage = (e) => {
-    e.preventDefault();
-    if (messageContentInput.value.length > 0) {
-        addMessage(userName, messageContentInput.value);
-        messageContentInput.value = "";
-    } else {
-        alert('Please enter a message');
-    }
+  e.preventDefault();
+  if (messageContentInput.value.length > 0) {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = "";
+  } else {
+    alert("Please enter a message");
+  }
+};
+
+const addMessage = (author, content) => {
+  const message = document.createElement("li");
+  message.classList.add("message");
+  message.classList.add("message--received");
+  if (author === userName) message.classList.add("message--self");
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? "You" : author}</h3>
+    <div class="message__content">
+        ${content}
+    </div>
+    `;
+  messagesList.appendChild(message);
 };
 
 // Event listeners
 
-loginForm.addEventListener('submit', (e) => login(e));
-addMessageForm.addEventListener('submit', (e) => sendMessage(e));
+loginForm.addEventListener("submit", (e) => login(e));
+addMessageForm.addEventListener("submit", (e) => sendMessage(e));
