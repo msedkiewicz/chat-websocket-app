caa421259de318c3bc709cbeecabeebfb5163258
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 19:21:02 +0100

    Add sending message

--- client/app.js
+++ client/app.js
@@ -15,8 +15,27 @@ let userName = '';
 
 const login = (e) => {
     e.preventDefault();
-    userName = userNameInput.value;
-    loginForm.classList.remove('show');
-    messagesSection.classList.add('show');
-    addMessageForm.classList.add('show');
+    if (userNameInput.value.length > 0) {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+        addMessageForm.classList.add('show');
+    } else {
+        alert('Please enter a username');
+    }
 };
+
+const sendMessage = (e) => {
+    e.preventDefault();
+    if (messageContentInput.value.length > 0) {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = "";
+    } else {
+        alert('Please enter a message');
+    }
+};
+
+// Event listeners
+
+loginForm.addEventListener('submit', (e) => login(e));
+addMessageForm.addEventListener('submit', (e) => sendMessage(e));
