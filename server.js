34390938606ea61154948b1fb1af32c755a855f4
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 21:38:21 +0100

    Add socket.io monitoring

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
 const path = require('path');
+const io = socket(server);
 
 const messages = [];
 app.use(express.static(path.join(__dirname, '/client/')));
@@ -10,6 +15,9 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) });
+  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+  console.log('I\'ve added a listener on message and disconnect events \n');
 });
