014e48edf487e81dcbe6a166647e15f7df0f1ff9
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-29 21:45:50 +0100

    Code formatting

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
 const server = app.listen(8000, () => {
-  console.log('Server is running on Port:', 8000)
+  console.log("Server is running on Port:", 8000);
 });
-const path = require('path');
+const path = require("path");
 const io = socket(server);
 
 const messages = [];
-app.use(express.static(path.join(__dirname, '/client/')));
+app.use(express.static(path.join(__dirname, "/client/")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('New client! Its id – ' + socket.id);
-  socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) });
-  socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
-  console.log('I\'ve added a listener on message and disconnect events \n');
+io.on("connection", (socket) => {
+  console.log("New client! Its id - " + socket.id);
+  socket.on("message", (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit("message", message);
+  });
+  socket.on("disconnect", () => {
+    console.log("Oh, socket " + socket.id + " has left");
+  });
+  console.log("I've added a listener on message and disconnect events \n");
 });
